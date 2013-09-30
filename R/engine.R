769fb9cf4968dff04d1e3acef74eb87182362bf5
Robert Flight <r.flight79@gmail.com>
2013-09-30 11:44:26 -0400

    using the outside function calls for orphan pages and posts in refresh.site

--- R/engine.R
+++ R/engine.R
@@ -294,35 +294,6 @@ update.site <- function(site, site.state, post.layout, tag.layout, fig.path){
 #' @param fig.path name of the directory in the site where figures (particularly R charts etc.) are to be kept
 #' @return logical TRUE if site has been updated, FALSE otherwise
 refresh.site <- function(site, site.state, post.layout, tag.layout, fig.path){
-    ## combine into a single function? --
-    orphan.pages.p <- function(){
-        # html pages with no R source
-        if(length(site.state$dest_pages)){
-            sapply(1:length(site.state$dest_pages),
-                   function(x) !dp[x] %in% sp)
-        } else FALSE
-    }
-    
-    orphan.posts.p <- function(){
-        # html posts with no R source
-        if(length(site.state$dest_posts)){
-            sapply(1:length(site.state$dest_posts),
-                   function(x) !db[x] %in% sb)
-        } else FALSE
-    }
-    
-    check.layouts <- function(lays, states){
-        # are any layouts newer than any files in states?
-        for(l in lays){
-            if(any(l > states)) return(TRUE)
-        }
-        FALSE
-    }
-        catch_char_zero <- function(x){
-        if(!length(x))x <- ""
-        x
-    }
-    
     sp <- catch_char_zero(str_replace(as.character(sapply(names(site.state$source_pages), 
                                                           function(x) basename(x))),
                                       "\\.R", "\\.html")) # source pages
@@ -335,8 +306,8 @@ refresh.site <- function(site, site.state, post.layout, tag.layout, fig.path){
     
     db <- catch_char_zero(as.character(sapply(names(site.state$dest_posts), 
                                               function(x) basename(x)))) # dest blog posts
-    orphan.pages <- names(site.state$dest_pages[orphan.pages.p()])
-    orphan.posts <- names(site.state$dest_posts[orphan.posts.p()])
+    orphan.pages <- names(site.state$dest_pages[orphan.items(dp, sp)])
+    orphan.posts <- names(site.state$dest_posts[orphan.items(db, sb)])
     
     if(length(orphan.pages) || length(orphan.posts)){
         for(f in c(orphan.pages, orphan.posts)){
